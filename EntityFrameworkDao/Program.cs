46003badcc9244562cfdb833c6ecf10189ebbc2e
Soxxoro <soxxoro@users.noreply.github.com>
2026-05-27 10:03:39 +0530

    Configure EntityFrameworkDao as a separate console application with its own Main

---
+++ EntityFrameworkDao/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EntityFrameworkDao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoRunner.RunDemo();
+        }
+    }
+}
